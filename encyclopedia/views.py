95ad024c377790a32af53285ea2134a60f4c4438
ca-Stella <stella.2532@gmail.com>
2022-09-08 13:02:02 -0600

    Initial attempt to render each wiki page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown2 import Markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title): 
+    markdowner = Markdown()
+    page = util.get_entry(title)
+    return render(request, "encyclopedia/entry.html", {
+        "page": markdowner.convert(page), 
+        "title": title
+    })
