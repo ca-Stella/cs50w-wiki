a616ae2ba47a31fa68efa99b4b9e5975588287f4
ca-Stella <stella.2532@gmail.com>
2022-09-10 19:30:42 -0600

    Set up search page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name="entry"),
-    path("dne_error", views.entry, name="dne")
+    path("dne_error", views.entry, name="dne"),
+    path("search", views.search, name="search")
 ]
